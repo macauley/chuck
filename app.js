070a9d69bb853efc2918759ff01c813596ab99d8
Eric Macauley <eric@macauley.me>
2016-01-16 17:57:24 -0800

    Hook up to ICNDb.com to get the facts

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,17 +10,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // route
 app.post('/', function (req, res) {
-	var payload = {
-		"response_type": "in_channel",
-		"text": "Chuck Norris fact generator!",
-		"attachments": [
-			{
-				"text": "Chuck Norris is the fucking man!"
-			}
-		]
-	};
-
-	res.status(200).json(payload); 
+	// Fetch a random fact for Chuck!
+	request('http://api.icndb.com/jokes/random', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var payload = {
+				"response_type": "in_channel",
+				"text": JSON.parse(body).value.joke,
+				"attachments": [
+					{
+						"text": "Chuck Norris is the fucking man!"
+					}
+				]
+			};
+			res.status(200).json(payload); 
+		}
+		else {
+			res.status(respsonse.statusCode).response;
+		}
+	});
 });
 
 // error handler
