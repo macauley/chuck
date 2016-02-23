0287f2981218b0a0d8d6277a79260bee5dfcbeb9
Eric Macauley <eric@macauley.me>
2016-02-22 22:41:23 -0800

    Parse JSON from lebowski.me, add markdown for Slack formatting

--- app.js
+++ app.js
@@ -27,11 +27,13 @@ app.post('/', function (req, res) {
 
 app.post('/thedude', function (req, res) {
     // Fetch a random fact for Big Lebowski!
-    request('http://lebowski.me/api/quotes/random.txt', function (error, response, body) {
+    request('http://lebowski.me/api/quotes/random', function (error, response, body) {
         if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body);
+            var markdownQuote = joinlines(data['quote']['lines']);
             var payload = {
                 "response_type": "in_channel",
-                "text": "\n" + body,
+                "text": markdownQuote,
             };
             res.status(200).json(payload);
         }
@@ -67,3 +69,11 @@ app.use(function (err, req, res, next) {
 app.listen(port, function () {
     console.log('Slack bot listening on port ' + port);
 });
+
+var joinlines = function(lines) {
+  var ret = []
+  lines.forEach(function(line){
+    ret.push('*_' + line.character.name '_*' + '\n' + line.text);
+  });
+  return ret.join('\n\n') + '\n';
+};
